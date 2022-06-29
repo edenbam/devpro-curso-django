238965f50258708ca028c95a41f15d4efc924cc3
edenbam <edenbam@gmail.com>
2022-06-29 15:13:17 -0300

    Configurado User e UserManage

--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email,password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, email, password):
+        pass
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(("email address"), unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- pypro/settings.py
+++ pypro/settings.py
@@ -29,8 +29,9 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-# Application definition
+AUTH_USER_MODEL = 'base.User'
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
