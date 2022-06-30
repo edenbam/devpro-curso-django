0fc0c4a8aa4ce15467ae01bfbf2ee93269f7a0e7
edenbam <edenbam@gmail.com>
2022-06-30 13:27:19 -0300

    Configuração do AWS e S3

--- pypro/settings.py
+++ pypro/settings.py
@@ -136,24 +136,24 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False  # Não irá ser criado bucket automaticamente
     AWS_QUERYSTRING_AUTH = True  # Para gerar URL's assinadas
     AWS_S3_CUSTOM_DOMAIN = None  # Para utilizar o próprio domínio do S3
-    AWS_DEFAULT_ACL = 'private' # Para que os arquivos do S# não fiquem públicos
+    AWS_DEFAULT_ACL = 'private'  # Para que os arquivos do S# não fiquem públicos
 
     # Static Assets
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage' # É uma classe da lib django-s3-folder-storage que vai fazer a gestão da pasta static
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'  # É uma classe da lib django-s3-folder-storage que
+    # vai fazer a gestão da pasta static
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
     # Upload Media Folder
-    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage' # Classe que fai fazer a gestão de upload de arquivos
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'  # Classe que fai fazer a gestão de upload de arquivos
     DEFAULT_S3_PATH = 'media'
     MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
     MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
